00fd319f92924e5e22e274c200aa767be005671f
lamine <mba@gmail.com>
2023-11-16 03:32:21 +0100

    gg

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -4,7 +4,7 @@ login_page.html
 <html lang="en">
 <head>
   <meta charset="utf-8" />
-  <!-- <link rel="icon" href="/favicon.ico" /> -->
+  <!-- <link rel="icon" href="/favicon.ico" />gggggg -->
   <meta name="viewport" content="width=device-width, initial-scale=1" />
   <meta name="theme-color" content="#000000" />
   <title>login</title>
